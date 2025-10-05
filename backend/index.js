4184c1146a03bcf32cf67309062fb0ba40bad4b4
r1a2j3p4u5t <rajputgaurav8135@gmail.com>
2025-10-06 01:07:34 +0530

    production changes

--- backend/index.js
+++ backend/index.js
@@ -36,5 +36,5 @@ app.get("/", (req, res) => {
 
 server.listen(process.env.PORT, async () => {
   await connectDB();
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
