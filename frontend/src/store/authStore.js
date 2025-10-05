a2ee15e9e3439fbecf6cce2fd38b7b77844950c7
r1a2j3p4u5t <rajputgaurav8135@gmail.com>
2025-10-06 00:45:10 +0530

    production changes

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { axiosInstance } from '../lib/axios.js'
 import { io } from "socket.io-client"
 
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://chat-application-rsxd.onrender.com/"
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
