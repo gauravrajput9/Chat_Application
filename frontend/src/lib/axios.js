5285d828381b6a77a9ee6509b8ab4ef7a3afc39f
r1a2j3p4u5t <rajputgaurav8135@gmail.com>
2025-10-06 00:26:26 +0530

    Production Changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://chat-application-rsxd.onrender.com/api",
     withCredentials: true
 })
 
